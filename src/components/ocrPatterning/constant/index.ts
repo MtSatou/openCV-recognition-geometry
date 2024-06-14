d55584cbc110ce77108f8ec909dc59b9d9b03818
松坂砂糖 <2365999861@qq.com>
2024-06-14 20:31:08 +0800

    update: 添加枚举类型

---
+++ src/components/ocrPatterning/constant/index.ts
@@ -0,0 +1,12 @@
+export enum shapeTypesMap {
+  Triangle = "三角形",
+  Diamond = "菱形",
+  Square = "正方形",
+  Rectangle = "矩形",
+  Trapezoid = "梯形",
+  Pentagon = "五边形",
+  Star = "五角星",
+  Hexagon = "六边形",
+  Circle = "圆形",
+  Unknown = "未知"
+}
